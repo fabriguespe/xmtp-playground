bff78bfa5ab836e5a754dd65c06dceea09c36cf6
fabrizioguespe <fabrizioguespe@MacFabri.local>
2023-07-13 18:35:22 -0300

    new

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,62 @@
+import React, { useState } from "react";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 
 import { Wallet } from "ethers";
 import { Client } from "@xmtp/xmtp-js";
+import styles from "./index.module.css";
 
-const scope = { Wallet, Client };
+export default function Index() {
+  const [loggedStrings, setLoggedStrings] = useState([]); // state variable to store logged strings
 
-const code2 = `
+  const handleLog = (x) => {
+    x = JSON.stringify(x); //now scape this characters
+    x = x.replace(/\\n/g, " "); // replace \n with space
+    //replace also " and \ with nothing
+    x = x.replace(/"/g, "");
+    setLoggedStrings((oldLogs) => [...oldLogs, x]); // log function that updates state
+    console.log(x); // also log to console
+  };
+  const [textareaValue, setTextareaValue] = useState("log('x')"); // new state variable
+  const log = (x) => console.log(x);
+  const scope = { log: handleLog, LiveProvider, styles, Wallet, Client };
+  const handleTextareaChange = (event) => {
+    // new function to handle the textarea value change
+    setTextareaValue(event.target.value);
+  };
 
-
-const Counter = (props) => {
-
-  const [count, setCount] = React.useState<number>(0)
-  const doSomething = () => {
-    // You'll want to replace this with a wallet from your application
-    const wallet = Wallet.createRandom();
-    console.log(wallet)
+  const code2 = `
+  const XMTPCODE = (props) => {
+    const [count, setCount] = React.useState<number>(0)
+    
+    const doSomething = () => {
+      // User-provided code will go here
+      ${textareaValue}
+    }
+  
+    return (
+      <div>
+        <button onClick={doSomething}>  ▶️ RUN </button>
+        
+      <textarea className={styles.test} >\`${loggedStrings}\`</textarea>
+      </div>
+    )
   }
-  return (
+  
+  render(
     <div>
-      <h3> {props.label}: {count} 🧮   </h3>
-      <button onClick={doSomething}>  ▶️  </button>
+      <XMTPCODE />
     </div>
   )
-}
-render(
-<div>
-  <Counter label="Counter" />
-  <Counter label="Counter" />
-</div>
-)
-`;
-export default function Index() {
+  `;
+
   return (
     <div>
-      <LiveProvider code={code2} noInline={true} scope={scope}>
+      <textarea
+        value={textareaValue} // set textarea value from state
+        onChange={handleTextareaChange} // update state when textarea value changes
+        className={styles.test} // make the textarea full width
+      />
+      <LiveProvider code={code2} scope={scope} noInline={true}>
         <LiveEditor />
         <LiveError />
         <LivePreview />
